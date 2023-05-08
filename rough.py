6f05b934324fdb8a247cdd0b8c59a9f4a9464fc2
Ashimeon <chetriashim10@gmail.com>
2023-05-08 12:19:59 +0545

    done

--- rough.py
+++ rough.py
@@ -1,8 +1,2 @@
-class company:
-    def __init__(self):
-        name="tata"
-        age=7
-    def check(self):
-        print(f"{self.name} is {self.age} years old")
-c1=company()
-print(c1.check())
+import numpy as np
+print(np.ones((5,3,(2,3))))
