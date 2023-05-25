6e8fe7bece872873207b8bb0e81a0a04fa693b6d
Ashimeon <chetriashim10@gmail.com>
2023-05-25 12:15:09 +0545

    revise

--- class1.py
+++ class1.py
@@ -21,35 +21,61 @@ token: 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
 
 ### class 3
 '''
+1. first create link:
+
 git remote add origin https://github.com/Ashimeon/DATA-Science.git    "create file on remote server"
-git remote remove origin
+git remote -v
+git remote remove origin (remove link)
 
 git remote -v   "status link with remote"
-git status  "show all"
+
+# git status  "show all status"
+
 created a file ".gitignore" then add file to ignore
 
+
+2. second add file on link:
 
 git add class1.py "to track only file.......aba comit garna baki"
 git commit -m"tracked class1.py file"    "add file lost"
 
+3. third before adding:
 
 git log (commit history)
 git branch
 git branch -m master main (rename branch)
-git checkout -b master
+git checkout -b master (create branch)
 git checkout (branch name)  ----> goes to branchname
 git branch -d (branch name) -----> after selecting another branch
 
 git push -u origin (branch name "main") (push on remote)
 
 
-
+git commit
 "reset"
-soft reset: git reset --soft HEAD-1
+soft reset: git reset --soft HEAD-1 
+hard reset: git reset --hard HEAD-1
+git reset HEAD-1
+--->commit ma gako reset
 git restore --staged (file name)   --->add ma gako reset
 
 
+4. tanney
 
 git pull
 git pull origin branch-name
+'''
+
+
+
+'''
+5. for folder:
+
+mkdir ashim
+cd ashim
+# cd ..
+
+git log (commit history)
+
+q- for quit
 '''
